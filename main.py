8c474f929791b7d251f2d1598beb83459567f56e
Craner <iayfnp@gmail.com>
2024-09-16 14:01:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pyautogui as gui
 from datetime import datetime
 import keyboard
-import time
 
 while not keyboard.is_pressed('ESC'):
     if keyboard.is_pressed('SPACE'):
         x, y = gui.position()
         current_time = datetime.now()
         print(f"{current_time}: x pos is {x}, y pos is {y}")
-        while keyboard.is_pressed('SPACE'):
-            junk = "do nothing"
+        keyboard.wait('SPACE', suppress=True, trigger_on_release=True)
